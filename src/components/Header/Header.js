9cb9c306620aa61d25752a5336fb785d16317aaf
Shawn Ng <shawnngyongkwang@gmail.com>
2020-08-04 15:27:38 +0800

    Initialise app and create header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const header = () => {
+  return (
+    <div id="header-container">
+      <h1 id="title">A Nice RNG</h1>
+      <p id="desc">It's just an RNG, but at least it looks good</p>
+    </div>
+  );
+};
+
+export default header;
---
+++ src/components/RangeSetter/RangeSetter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const rangeSetter = () => {
+  return <p>stuff here</p>;
+};
+
+export default rangeSetter;
